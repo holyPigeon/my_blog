6a68f64322d9159b60ee5f2096ea6c1cdf22ae63
jae woo Jung <awfjol2008@naver.com>
2023-03-07 02:01:15 +0900

    feat: MemberAPIController 회원 조회 API 추가
    "/member/list" url GET 요청에 따른 회원의 id와 name값을 반환함

--- src/main/java/com/example/my_blog/api/MemberAPIController.java
+++ src/main/java/com/example/my_blog/api/MemberAPIController.java
@@ -5,9 +5,13 @@ import com.example.my_blog.domain.member.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +20,9 @@ public class MemberAPIController {
   private final MemberService memberService;
 
   /**
-   * 회원가입
+   * 회원 가입
    */
-  @PostMapping("/mebmer/join")
+  @PostMapping("/member/join")
   public JoinMemberResponse joinMember(@RequestBody JoinMemberRequest request) {
     Member member = new Member();
     member.setName(request.getName());
@@ -26,6 +30,18 @@ public class MemberAPIController {
     memberService.join(member);
 
     return new JoinMemberResponse(member.getId());
+  }
+
+  /**
+   * 회원 조회
+   */
+  @GetMapping("/member/list")
+  public ListMemberResponse<List<MemberData>> listMember() {
+    List<Member> memberList = memberService.findAll();
+    List<MemberData> memberData = memberList.stream().map(m -> new MemberData(m.getId(), m.getName()))
+        .collect(Collectors.toList());
+
+    return new ListMemberResponse<>(memberData.size(), memberData);
   }
 
   @Data
@@ -39,5 +55,21 @@ public class MemberAPIController {
   static class JoinMemberResponse {
 
     private Long id;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class ListMemberResponse<T> {
+
+    private int count;
+    private T data;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class MemberData {
+
+    private Long id;
+    private String name;
   }
 }
