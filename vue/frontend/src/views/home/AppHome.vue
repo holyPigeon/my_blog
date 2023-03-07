060a609f1e937873823c0f47701c1732ad4af5d3
jae woo Jung <awfjol2008@gmail.com>
2023-03-07 19:20:17 +0900

    [FE] 메인 페이지 및 회원 가입, 회원 조회 페이지 구현 (#1)
    * feat: HelloWorld.vue 추가
    
    메인페이지 역할의 HelloWorld.vue 추가
    
    * feat: HelloWorld.vue 삭제
    
    홈 화면 지정을 위해 HelloWorld.vue 삭제
    
    * feat: 회원가입 페이지 추가
    
    Member의 name 정보를 request 값으로 받는 회원가입 페이지 추가
    
    * feat: 회원가입 페이지 라우팅 경로 설정 추가
    
    MemberJoin.vue 의 라우팅 경로를 '/member/join'으로 추가
    
    * feat: 홈 화면 추가
    
    AppHome.vue를 전체 앱의 홈 화면으로 추가함
    
    * feat: 회원가입 페이지 Vue 이미지 추가
    
    * feat: 회원목록 조회 페이지 추가
    
    axios 요청을 통해 회원 아이디와 이름 정보를 조회
    
    * feat: 회원조회 페이지 라우팅 경로 설정 추가
    
    MemberList.vue의 라우팅 경로를 '/member/list'으로 추가
    
    * feat: vue-router 사용을 위한 App.vue 파일 수정
    
    기존 컴포넌트인 HelloWorld 삭제 후 router-view 태그 추가
    
    * feat: 회원조회 페이지 API 요청 로직 추가
    
    beforeMount 내에서 회원 조회 요청 후 반환값을 memberList 데이터에 저장
    
    * feat: 회원조회 요청 로직 변경
    
    request data 제거 및 저장 반환값 변경 (res.data -> res.data.data)

---
+++ vue/frontend/src/views/home/AppHome.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../../assets/logo.png">
+    <h1></h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3><a href="#" @click="$router.push('/')" class="nav-link active">This is Home</a></h3>
+    <div class="row g-3">
+      <div class="col-md-2 offset-md-4">
+        <!-- <button @click="login" class="btn btn-outline-primary btn-lg col-md-12 p-2">로그인</button> -->
+        <h4 @click="$router.push('/member/join')"><button class="btn btn-outline-primary py-3 m-3">회원 가입</button></h4>
+      </div>
+      <div class="col-md-2">
+        <h4 @click="$router.push('/member/list')"><button class="btn btn-outline-primary py-3 m-3">회원 조회</button></h4>
+      </div>
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'appHome',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped></style>
