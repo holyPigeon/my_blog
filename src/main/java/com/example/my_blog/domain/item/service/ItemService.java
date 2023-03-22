fbe20c5b38076f33808f57e01caffb4950669c77
jae woo Jung <awfjol2008@naver.com>
2023-03-22 11:19:10 +0900

    feat: ItemRepositoryImpl 추가
    ItemRepository의 구현체인 ItemRepositoryImpl 추가

---
+++ src/main/java/com/example/my_blog/domain/item/service/ItemService.java
@@ -0,0 +1,56 @@
+package com.example.my_blog.domain.item.service;
+
+import com.example.my_blog.domain.item.Item;
+import com.example.my_blog.domain.item.repository.ItemRepository;
+import com.example.my_blog.domain.item.service.dto.request.ItemEditRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+  private final ItemRepository itemRepository;
+
+  @Transactional
+  public void save(Item item) {
+    itemRepository.save(item);
+  }
+
+  public Item findById(Long id) {
+    return itemRepository.findById(id);
+  }
+
+  public Item findByName(String name) {
+    return itemRepository.findByName(name);
+  }
+
+  public List<Item> findByMemberId(Long memberId) {
+    return itemRepository.findByMemberId(memberId);
+  }
+
+  public List<Item> findAll() {
+    return itemRepository.findAll();
+  }
+
+  @Transactional
+  public void update(Long id, ItemEditRequestDTO itemEditRequestDTO) {
+    Item findItem = findById(id);
+
+    if (findItem!= null) {
+      findItem.setName(itemEditRequestDTO.getName());
+      findItem.setPrice(itemEditRequestDTO.getPrice());
+      findItem.setQuantity(itemEditRequestDTO.getQuantity());
+    }
+  }
+
+  public void deleteById(Long id) {
+    itemRepository.deleteById(id);
+  }
+
+
+}
