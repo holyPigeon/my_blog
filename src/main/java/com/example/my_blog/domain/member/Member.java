50aea4848b0aa6e61d1cec577e7e82731c05d2e1
jae woo Jung <awfjol2008@naver.com>
2023-03-22 11:24:30 +0900

    feat: Member와 Item간의 연관관계 설정
    Member와 Item간의 일대다 연관관게 설정

--- src/main/java/com/example/my_blog/domain/member/Member.java
+++ src/main/java/com/example/my_blog/domain/member/Member.java
@@ -1,8 +1,12 @@
 package com.example.my_blog.domain.member;
 
+import com.example.my_blog.domain.item.Item;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -13,4 +17,7 @@ public class Member {
   private Long id;
 
   private String name;
+
+  @OneToMany(mappedBy = "member")
+  private List<Item> items = new ArrayList<>();
 }
