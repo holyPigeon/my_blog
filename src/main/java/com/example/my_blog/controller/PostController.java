bad5c0c8afe89e74ec13d922afb7f0ab7fee7f30
jae woo Jung <awfjol2008@naver.com>
2023-04-03 00:05:38 +0900

    fix: DTO 스펙 변경에 따른 API 수정
    게시글 등록 Request DTO 스펙 변경에 따라서 findByName 메소드를 통해 Member를 가져와 게시글 생성
    게시글 조회 Response DTO 스펙 변경으로 인해 Member가 아닌 Member.getName() 입력

--- src/main/java/com/example/my_blog/controller/PostController.java
+++ src/main/java/com/example/my_blog/controller/PostController.java
@@ -1,5 +1,7 @@
 package com.example.my_blog.controller;
 
+import com.example.my_blog.domain.member.Member;
+import com.example.my_blog.domain.member.service.MemberService;
 import com.example.my_blog.domain.post.Post;
 import com.example.my_blog.domain.post.service.PostService;
 import com.example.my_blog.domain.post.service.dto.request.CreatePostRequestDTO;
@@ -19,13 +21,16 @@ import java.util.List;
 public class PostController {
 
   private final PostService postService;
+  private final MemberService memberService;
 
   @PostMapping("/post/create")
   public CreatePostResponseDTO registerItem(@RequestBody CreatePostRequestDTO createPostRequestDTO) {
 
-    Post post = Post.createPost(createPostRequestDTO.getMember(), createPostRequestDTO.getTitle(), createPostRequestDTO.getContent());
+    Member findMember = memberService.findByName(createPostRequestDTO.getAuthor());
+    Post post = Post.createPost(findMember, createPostRequestDTO.getTitle(), createPostRequestDTO.getContent());
+    Long postId = postService.save(post);
 
-    return new CreatePostResponseDTO(post.getId());
+    return new CreatePostResponseDTO(postId);
 
   }
 
@@ -34,7 +39,8 @@ public class PostController {
     List<Post> posts = postService.findAll();
 
     List<ListPostResponseDTO> collect = posts.stream().map(p ->
-        new ListPostResponseDTO(p.getId(), p.getMember(), p.getTitle(), p.getContent())).toList();
+        new ListPostResponseDTO(p.getId(), p.getMember().getName(), p.getTitle(), p.getContent(),
+            p.getCreatedAt(), p.getUpdatedAt())).toList();
 
     return new ListPostResponse<>(collect.size(), collect);
 
