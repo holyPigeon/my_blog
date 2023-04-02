5ae21c6deccc6f3f0e8f0d30853051d5e5853dfa
jae woo Jung <awfjol2008@naver.com>
2023-04-02 23:49:10 +0900

    feat: 게시글 작성 관련 Request DTO 수정
    Member 타입 변수 관련하여 오류 발생으로 String 타입의 author 변수로 변경

--- src/main/java/com/example/my_blog/domain/post/service/dto/request/CreatePostRequestDTO.java
+++ src/main/java/com/example/my_blog/domain/post/service/dto/request/CreatePostRequestDTO.java
@@ -1,6 +1,5 @@
 package com.example.my_blog.domain.post.service.dto.request;
 
-import com.example.my_blog.domain.member.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CreatePostRequestDTO {
 
-  private Member member;
+  private String author;
   private String title;
   private String content;
 }
