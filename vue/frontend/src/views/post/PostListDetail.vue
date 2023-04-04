be1ea51056a06736e51e9803963ed616c724afb1
jae woo Jung <awfjol2008@naver.com>
2023-04-04 10:15:17 +0900

    feat: 상세 글 페이지 UI 추가
    해당 글 제목과 본문 등 상세 내용 조회 기능

---
+++ vue/frontend/src/views/post/PostListDetail.vue
@@ -0,0 +1,140 @@
+<template>
+    <div id="wrapper" class="container-fluid">
+        <!-- <img alt="Vue logo" src="../../assets/logo.png"> -->
+        <div class="container px-4s my-5">
+            <div class="row gx-4 justify-content-center m-auto">
+                <div class="col-md-10 position-static d-block p-3 text-black"
+                    style="--bs-bg-opacity: .4; --bs-text-opacity: 0.6;">
+                    <div class="container mt-5">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <!-- Post content-->
+                                <article>
+                                    <!-- Post header-->
+                                    <header class="mb-4">
+                                        <!-- Post title-->
+                                        <h1 class="fw-bolder mb-1">{{ postData.title }}</h1><!-- Post meta content-->
+                                        <div class="text-muted fst-italic mb-2">Posted on {{ postData.createdAt }} by {{ postData.author }}
+                                        </div>
+                                    </header><!-- Preview image figure-->
+                                    <!-- <figure class="mb-4"><img class="img-fluid rounded"
+                                            src="https://dummyimage.com/900x400/ced4da/6c757d.jpg" alt="..."></figure> -->
+                                    <!-- Post content-->
+                                    <section class="mb-5">
+                                        <p class="fs-5 mb-4">{{ postData.content }}</p>
+                                    </section>
+                                </article>
+                                <!-- <section class="mb-5">
+                                    <div class="card bg-light">
+                                        <div class="card-body">
+                                            <form class="mb-4"><textarea class="form-control" rows="3"
+                                                    placeholder="Join the discussion and leave a comment!"></textarea>
+                                            </form>
+                                            <div class="d-flex mb-4">
+                                            
+                                                <div class="flex-shrink-0"><img class="rounded-circle"
+                                                        src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="...">
+                                                </div>
+                                                <div class="ms-3">
+                                                    <div class="fw-bold">Commenter Name</div>If you're going to lead a space
+                                                    frontier, it has to be government; it'll never be private enterprise.
+                                                    Because the space frontier is dangerous, and it's expensive, and it has
+                                                    unquantified risks.
+                                                  
+                                                    <div class="d-flex mt-4">
+                                                        <div class="flex-shrink-0"><img class="rounded-circle"
+                                                                src="https://dummyimage.com/50x50/ced4da/6c757d.jpg"
+                                                                alt="..."></div>
+                                                        <div class="ms-3">
+                                                            <div class="fw-bold">Commenter Name</div>And under those
+                                                            conditions, you cannot establish a capital-market evaluation of
+                                                            that enterprise. You can't get investors.
+                                                        </div>
+                                                    </div>
+                                                    <div class="d-flex mt-4">
+                                                        <div class="flex-shrink-0"><img class="rounded-circle"
+                                                                src="https://dummyimage.com/50x50/ced4da/6c757d.jpg"
+                                                                alt="..."></div>
+                                                        <div class="ms-3">
+                                                            <div class="fw-bold">Commenter Name</div>When you put money
+                                                            directly to a problem, it makes a good headline.
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex">
+                                                <div class="flex-shrink-0"><img class="rounded-circle"
+                                                        src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="...">
+                                                </div>
+                                                <div class="ms-3">
+                                                    <div class="fw-bold">Commenter Name</div>When I look at the universe and
+                                                    all the ways the universe wants to kill us, I find it hard to reconcile
+                                                    that with statements of beneficence.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'postListDetail',
+    data() {
+        return {
+            postId: -1,
+            postData: {
+                // id: '',
+                // name: '',
+                // price: -1,
+                // quantity: -1,
+            }
+        }
+    },
+    methods: {
+        test() {
+            const url = window.location.pathname;
+            const splitArr = url.split("/");
+            const postId = splitArr[splitArr.length - 1]; // "1"
+            this.postId = postId;
+            console.log("postId -> " + postId);
+        }
+    },
+    beforeMount() {
+        const url = window.location.pathname;
+        const splitArr = url.split("/");
+        const postId = splitArr[splitArr.length - 1]; // "1"
+        this.postId = postId;
+        console.log("postId -> " + postId);
+
+        axios.get(`/post/list/${this.postId}`)
+            .then((res) => {
+                console.log("res.data => " + JSON.stringify(res.data));
+                this.postData = {...res.data};
+                console.log("postData -> " + JSON.stringify(this.postData));
+            }).catch((err) => {
+                JSON.stringify("err => " + err);
+            });
+    },
+}
+
+</script>
+ 
+<style>
+input::placeholder {
+    font-size: 14px;
+}
+
+td {
+    padding: 5px;
+}
+</style>
