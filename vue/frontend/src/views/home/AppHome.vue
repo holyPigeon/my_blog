dd3514ed3fe0d1aa1281da72d67d86afcdd70e1a
jae woo Jung <awfjol2008@naver.com>
2023-04-06 22:51:07 +0900

    fix: 불필요한 스타일 코드 제거

--- vue/frontend/src/views/home/AppHome.vue
+++ vue/frontend/src/views/home/AppHome.vue
@@ -7,38 +7,37 @@
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3><a href="#" @click="$router.push('/')" class="nav-link active">This is Home</a></h3>
-    <div class="row g-3">
-      <div class="col-md-6 offset-md-3">
-        <div class="row mt-3">
+    <div class="row g-3 mt-3">
 
-          <div class="col-md-4 offset-md-4">
-            <div class="btn-group">
-              <button class="btn btn-outline-primary py-3">
-                <h6 @click="$router.push('/member/join')">회원 가입</h6>
-              </button>
-              <button class="btn btn-outline-primary py-3">
-                <h6 @click="$router.push('/member/list')">회원 조회</h6>
-              </button>
-            </div>
-          </div>
+      <div class="col-md-4 offset-md-4">
+        <div class="btn-group">
+          <button class="btn btn-outline-primary py-3">
+            <h6 @click="$router.push('/member/join')">회원 가입</h6>
+          </button>
+          <button class="btn btn-outline-primary py-3">
+            <h6 @click="$router.push('/member/list')">회원 조회</h6>
+          </button>
         </div>
       </div>
-      <div class="col-md-6 offset-md-3">
-        <div class="row">
 
-          <div class="col-md-4 offset-md-4">
-            <div class="btn-group">
-              <button class="btn btn-outline-primary py-3">
-                <h6 @click="$router.push('/post/create')">게시글 등록</h6>
-              </button>
-              <button class="btn btn-outline-primary py-3">
-                <h6 @click="$router.push('/post/list')">게시글 조회</h6>
-              </button>
-            </div>
-          </div>
+      <div class="col-md-4 offset-md-4">
+        <div class="btn-group">
+          <button class="btn btn-outline-primary py-3">
+            <h6 @click="$router.push('/post/create')">게시글 등록</h6>
+          </button>
+          <button class="btn btn-outline-primary py-3">
+            <h6 @click="$router.push('/post/list')">게시글 조회</h6>
+          </button>
         </div>
+      </div>
+
+      <div class="col-md-4 offset-md-4">
+        <button class="btn btn-outline-primary py-3">
+          <h6 @click="$router.push('/admin')">관리자 콘솔</h6>
+        </button>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -53,7 +52,7 @@ export default {
   },
   methods: {
     // test() {
-    //   const postId = window.location.pathname;
+    //   const postId = window.location.pathname; 
     //   console.log("postId => " + postId);
     // }
   }
