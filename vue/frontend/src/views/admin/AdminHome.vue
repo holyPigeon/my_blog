1bb523937983e5a318887c79a4ad635db5dc0eb4
jae woo Jung <awfjol2008@naver.com>
2023-04-07 22:27:30 +0900

    feat: 관리자 콘솔 홈 화면 UI 구현
    회원 관리 및 게시글 관리 기능을 포함한 관리자 콘솔의 홈 화면

---
+++ vue/frontend/src/views/admin/AdminHome.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1></h1>
+        <p>
+            For a guide and recipes on how to configure / customize this project,<br>
+            check out the
+            <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+        </p>
+        <h3><a href="#" @click="$router.push('/')" class="nav-link active">This is Admin Home</a></h3>
+        <div class="row g-3 mt-3">
+            <div class="col-md-4 offset-md-4">
+                <div class="btn-group">
+                    <button class="btn btn-outline-primary py-3">
+                        <h6 @click="$router.push('/admin/member')">회원 관리</h6>
+                    </button>
+                </div>
+            </div>
+            <div class="col-md-4 offset-md-4">
+                <div class="btn-group">
+                    <button class="btn btn-outline-primary py-3">
+                        <h6 @click="$router.push('/admin/post')">게시글 관리</h6>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+  
+<script>
+
+export default {
+    name: 'adminHome',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        // test() {
+        //   const postId = window.location.pathname; 
+        //   console.log("postId => " + postId);
+        // }
+    }
+}
+</script>
+  
+<style scoped></style>
+  
