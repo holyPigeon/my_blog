5712d824a3107391b6c33a8e151bff02433e4dfb
jae woo Jung <awfjol2008@naver.com>
2023-04-10 02:39:07 +0900

    feat: 회원 삭제 Response DTO 추가

---
+++ src/main/java/com/example/my_blog/domain/member/service/dto/response/DeleteMemberResponseDTO.java
@@ -0,0 +1,10 @@
+package com.example.my_blog.domain.member.service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DeleteMemberResponseDTO {
+  private Long id;
+}
