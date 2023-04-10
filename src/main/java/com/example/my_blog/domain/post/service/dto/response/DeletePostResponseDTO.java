1efbab61a9047631afeca3476ccffe1d8c614564
jae woo Jung <awfjol2008@naver.com>
2023-04-10 19:22:27 +0900

    feat: 게시글 삭제 Response DTO 추가

---
+++ src/main/java/com/example/my_blog/domain/post/service/dto/response/DeletePostResponseDTO.java
@@ -0,0 +1,10 @@
+package com.example.my_blog.domain.post.service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DeletePostResponseDTO {
+  private Long id;
+}
