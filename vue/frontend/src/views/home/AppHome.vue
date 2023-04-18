f05b863811f056b442587f28b7ad2befe14a2fea
jae woo Jung <awfjol2008@naver.com>
2023-04-18 09:15:27 +0900

    feat: 홈 화면 UI 변경 및 로그인 여부에 따른 정보 노출 구현

--- vue/frontend/src/views/home/AppHome.vue
+++ vue/frontend/src/views/home/AppHome.vue
@@ -10,18 +10,34 @@
     <div class="row g-3 mt-3">
 
       <div class="col-md-4 offset-md-4">
+        <h4 class="py-3">로그인한 사용자:
+          <span>
+            <h6 v-if="Object.keys(sessionData).length !== 0">{{ sessionData.name }} | {{ sessionData.age }}</h6>
+            <h6 v-if="Object.keys(sessionData).length === 0">없음</h6>
+          </span>
+        </h4>
+      </div>
+
+      <div class="col-md-8 offset-md-2">
         <div class="btn-group">
-          <button @click="$router.push('/member/join')" class="btn btn-outline-primary py-3">
+          <button v-if="Object.keys(sessionData).length === 0" @click="login" class="btn btn-outline-success py-3">
+            <h6>로그인</h6>
+          </button>
+          <button v-if="Object.keys(sessionData).length === 0" @click="$router.push('/member/join')" class="btn btn-outline-success py-3">
             <h6>회원 가입</h6>
           </button>
-          <button @click="$router.push('/member/list')" class="btn btn-outline-primary py-3">
-            <h6>회원 조회</h6>
+          <button v-if="Object.keys(sessionData).length !== 0" @click="logout" class="btn btn-outline-success py-3">
+            <h6>로그아웃</h6>
           </button>
+          
         </div>
       </div>
 
       <div class="col-md-4 offset-md-4">
         <div class="btn-group">
+          <button @click="$router.push('/member/list')" class="btn btn-outline-primary py-3">
+            <h6>회원 조회</h6>
+          </button>
           <button @click="$router.push('/post/create')" class="btn btn-outline-primary py-3">
             <h6>게시글 등록</h6>
           </button>
@@ -32,7 +48,7 @@
       </div>
 
       <div class="col-md-4 offset-md-4">
-        <button @click="$router.push('/admin')" class="btn btn-outline-primary py-3">
+        <button @click="$router.push('/admin')" class="btn btn-outline-danger py-3">
           <h6>관리자 콘솔</h6>
         </button>
       </div>
@@ -42,19 +58,51 @@
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
   name: 'appHome',
   data() {
     return {
-
+      isLogined: true,
+      sessionData: {
+        // id: 1,
+        // loginId: admin,
+        // name: kevin,
+        // age: 20,
+      }
     }
   },
   methods: {
-    // test() {
-    //   const postId = window.location.pathname; 
-    //   console.log("postId => " + postId);
-    // }
+    login() {
+      this.$router.push('/login')
+    },
+    logout() {
+      axios.post(`/logout`)
+      .then((res) => {
+        console.log(res);
+        location.reload();
+      }).catch((err) => {
+        let errMsg = JSON.stringify(err.response.data.message);
+        errMsg = errMsg.substring(1, errMsg.length - 1);
+        console.log("errMsg -> " + errMsg);
+        alert(errMsg);
+      });
+    },
+  },
+  beforeMount() {
+    axios.get(`/session`)
+      .then((res) => {
+        console.log(res);
+        if (res.data.id != -1) {
+          this.sessionData = { ...res.data };
+        }
+      }).catch((err) => {
+        let errMsg = JSON.stringify(err.response.data.message);
+        errMsg = errMsg.substring(1, errMsg.length - 1);
+        console.log("errMsg -> " + errMsg);
+        alert(errMsg);
+      });
   }
 }
 </script>
