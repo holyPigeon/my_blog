59e7c4edff2d0485966103ebaf459eebff17eb2a
jae woo Jung <awfjol2008@naver.com>
2023-06-09 21:31:16 +0900

    refector: 댓글 생성 Request DTO명 변경

--- src/main/java/com/example/my_blog/domain/comment/service/dto/request/CreateCommentRequest.java
+++ src/main/java/com/example/my_blog/domain/comment/service/dto/request/CreateCommentRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateCommentRequestDTO {
+public class CreateCommentRequest {
 
   private Long userId;
 
