7128991e82b159e26f2beb673a702a0fc091020f
jae woo Jung <awfjol2008@naver.com>
2023-06-09 21:35:25 +0900

    feat: User 엔티티 댓글 변수 추가

--- src/main/java/com/example/my_blog/domain/user/User.java
+++ src/main/java/com/example/my_blog/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.example.my_blog.domain.user;
 
+import com.example.my_blog.domain.comment.Comment;
 import com.example.my_blog.domain.post.Post;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -28,6 +29,6 @@ public class User {
   @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
   private List<Post> posts = new ArrayList<>();
 
-//  @OneToMany(mappedBy = "member")
-//  private List<Item> items = new ArrayList<>();
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+  private List<Comment> comments = new ArrayList<>();
 }
