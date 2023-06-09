f02e6e3d7b95b56c43add35a1238439477884c9a
jae woo Jung <awfjol2008@naver.com>
2023-06-09 21:17:03 +0900

    refector: MemberRepository UserRepository로 클래스명 변경

---
+++ src/main/java/com/example/my_blog/domain/user/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.example.my_blog.domain.user.repository;
+
+import com.example.my_blog.domain.user.User;
+
+import java.util.List;
+
+public interface UserRepository {
+
+  void save(User user);
+
+  User findById(Long id);
+
+  User findByLoginId(String loginId);
+
+  User findByName(String name);
+
+  List<User> findAll();
+
+  void deleteById(Long id);
+}
