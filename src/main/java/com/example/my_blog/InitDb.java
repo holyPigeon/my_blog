a4142a0df31421c44c799a27641f8be9ce70d4a8
jae woo Jung <awfjol2008@naver.com>
2023-06-10 23:16:29 +0900

    feat: 초기 DB 더미 데이터 주입 컴포넌트 구현

---
+++ src/main/java/com/example/my_blog/InitDb.java
@@ -0,0 +1,74 @@
+package com.example.my_blog;
+
+import com.example.my_blog.domain.comment.Comment;
+import com.example.my_blog.domain.post.Post;
+import com.example.my_blog.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+  private final InitService initService;
+
+  /**
+   * 애플리케이션 실행 전에 미리 init() 실행
+   */
+  @PostConstruct
+  public void init() {
+    initService.dbInit();
+  }
+
+  @Component
+  @Transactional
+  @RequiredArgsConstructor
+  @Slf4j
+  static class InitService {
+
+    private final EntityManager em;
+
+    public void dbInit() {
+
+      User user1 = createUser("admin", "0000", "admin", "Admin");
+      User user2 = createUser("test1", "test1", "test1", "中村 あげは");
+      User user3 = createUser("test2", "test2", "test2", "조병찬");
+      em.persist(user1);
+      em.persist(user2);
+      em.persist(user3);
+
+      Post post1 = createPost(user1, "테스트 게시물", "안녕하세요? 포럼 테스트용 게시물입니다. 포럼은 챗봇으로 해결되지 않는 문제를 집단지성으로 해결할 수 있는 공간입니다.");
+      Post post2 = createPost(user2, "I'm from japan", "頭がとても痛いのですが、どこに行けばいいですか?");
+      em.persist(post1);
+      em.persist(post2);
+
+      Comment comment1 = createComment(user2, post1, "こんにちは");
+      Comment comment2 = createComment(user3, post1, "반갑습니다.");
+      em.persist(comment1);
+      em.persist(comment2);
+
+      em.flush();
+    }
+
+    private User createUser(String loginId, String password, String name, String nickname) {
+
+      return User.createUser(loginId, password, name, nickname);
+    }
+
+    private Post createPost(User user, String title,String content) {
+
+      return Post.createPost(user, title, content);
+    }
+
+    private Comment createComment(User user, Post post, String content) {
+
+      return Comment.createComment(user, post, content);
+    }
+
+  }
+}
