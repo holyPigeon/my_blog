417118f5e3584c4b4404ee6b9abb254ddac8176b
jae woo Jung <awfjol2008@naver.com>
2023-06-11 21:48:05 +0900

    feat: 로그인 시 세션 정보를 세션 스토리지에 저장하도록 수정

--- vue/frontend/src/views/user/UserLogin.vue
+++ vue/frontend/src/views/user/UserLogin.vue
@@ -8,29 +8,29 @@
                         <!-- style="--bs-text-opacity: 0.6;" -->
                         <h4 class="mb-3 text-center text-black fs-3 fw-bold">로그인</h4>
                     </div>
-                        <div class="row g-3">
-                            <div class="col-md-4 offset-md-4">
-                                <input type="text" v-model="formData.loginId" class="form-control form-control-lg p-2"
-                                    id="loginId" name="loginId" placeholder="아이디" required>
-                            </div>
+                    <div class="row g-3">
+                        <div class="col-md-4 offset-md-4">
+                            <input type="text" v-model="formData.loginId" class="form-control form-control-lg p-2"
+                                id="loginId" name="loginId" placeholder="아이디" required>
                         </div>
-                        <div class="row g-3 mt-2">
-                            <div class="col-md-4 offset-md-4">
-                                <input type="text" v-model="formData.password" class="form-control form-control-lg p-2"
-                                    id="password" name="password" placeholder="패스워드" required>
-                            </div>
+                    </div>
+                    <div class="row g-3 mt-2">
+                        <div class="col-md-4 offset-md-4">
+                            <input type="text" v-model="formData.password" class="form-control form-control-lg p-2"
+                                id="password" name="password" placeholder="패스워드" required>
                         </div>
+                    </div>
 
-                        <!-- <div class="row">
+                    <!-- <div class="row">
                             <hr class="my-4 col-md-12 offset-md-0 border border-1 border-dark" style="opacity: 0.1;">
                         </div> -->
 
-                        <div class="row g-3 mt-3">
-                            <button type="button" @click="submitForm($router)"
-                                class="btn btn-outline-dark btn-lg col-md-4 offset-md-4 p-2" id="signup"
-                                style="opacity: 0.7;">완료</button>
-                        </div>
+                    <div class="row g-3 mt-3">
+                        <button type="button" @click="submitForm($router)"
+                            class="btn btn-outline-dark btn-lg col-md-4 offset-md-4 p-2" id="signup"
+                            style="opacity: 0.7;">완료</button>
                     </div>
+                </div>
             </div>
         </div>
     </div>
@@ -47,6 +47,12 @@ export default {
                 loginId: '',
                 password: '',
             },
+            sessionData: {
+                // id: 1,
+                // loginId: "",
+                // name: "",
+                // nickname: ""
+            }
         }
     },
     methods: {
@@ -55,6 +61,10 @@ export default {
             axios.post('/login', data)
                 .then((res) => {
                     console.log(res);
+                    sessionStorage.setItem("id", res.data.id);
+                    sessionStorage.setItem("loginId", res.data.loginId);
+                    sessionStorage.setItem("name", res.data.name);
+                    sessionStorage.setItem("nickname", res.data.nickname);
                     this.$router.push('/');
                 }).catch((err) => {
                     let errMsg = JSON.stringify(err.response.data.message);
