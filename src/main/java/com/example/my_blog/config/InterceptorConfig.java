25aeaf02d543519a88bfe52398cd68dd67c73a98
jae woo Jung <awfjol2008@naver.com>
2023-06-20 02:53:10 +0900

    style: WebConfig 클래스명 InterceptorConfig로 변경

--- src/main/java/com/example/my_blog/config/InterceptorConfig.java
+++ src/main/java/com/example/my_blog/config/InterceptorConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class InterceptorConfig implements WebMvcConfigurer {
 
   public void addInterceptors(InterceptorRegistry registry) {
     // 인터셉터 추가 로직 구현
