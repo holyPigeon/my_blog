5c69a5485706ab6873fbfd27d5706f0f183c6fa3
jae woo Jung <awfjol2008@naver.com>
2023-06-20 20:31:14 +0900

    feat: RequestInterceptor 구현
    세션 유무에 따라 API 요청을 거부

---
+++ src/main/java/com/example/my_blog/config/RequestInterceptor.java
@@ -0,0 +1,45 @@
+package com.example.my_blog.config;
+
+import com.example.my_blog.exception.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.Objects;
+
+import static com.example.my_blog.SessionConst.SESSION_KEY;
+import static com.example.my_blog.exception.ErrorCode.AUTHENTICATION_USER;
+
+
+@Slf4j
+public class RequestInterceptor implements HandlerInterceptor{
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    String requestURI = request.getRequestURI();
+    log.info("요청 URI : {}", requestURI);
+
+    HttpSession session = request.getSession(false);
+    if (Objects.isNull(session) || Objects.isNull(session.getAttribute(SESSION_KEY))) {
+      if (Objects.equals(request.getMethod(), "GET") && PatternMatchUtils.simpleMatch("/api/art/**", request.getRequestURI())) {
+        return true;
+      }
+
+      log.info("미인증 사용자 요청");
+      String errorResponseToJSON = objectMapper.writeValueAsString(ErrorResponse.of(AUTHENTICATION_USER));
+
+      response.setStatus(AUTHENTICATION_USER.getStatus().value());
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+      response.getWriter().write(errorResponseToJSON);
+      return false;
+    }
+
+    return true;
+  }
+}
