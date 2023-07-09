402a1dea32b4dde2e17e3c4c33f3897b18dd149f
jae woo Jung <awfjol2008@naver.com>
2023-07-09 20:50:43 +0900

    fix: 관리자 홈 페이지에 가로 스크롤이 생기는 UI 버그 수정

--- vue/frontend/src/views/admin/AdminHome.vue
+++ vue/frontend/src/views/admin/AdminHome.vue
@@ -1,25 +1,28 @@
 <template>
     <div>
-        <h1></h1>
-        <p>
-            For a guide and recipes on how to configure / customize this project,<br>
-            check out the
-            <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-        </p>
-        <h3><a href="#" @click="$router.push('/')" class="nav-link active">This is Admin Home</a></h3>
-        <div class="row g-3 mt-3">
-            <div class="col-md-4 offset-md-4">
-                <div class="btn-group">
-                    <button @click="$router.push('/admin/user')" class="btn btn-outline-danger py-3">
-                        <h6>회원 관리</h6>
-                    </button>
+        <div style="margin-top: 100px;">
+            <p>
+                For a guide and recipes on how to configure / customize this project,<br>
+                check out the
+                <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+            </p>
+            <h3><a href="#" @click="$router.push('/')" class="nav-link active">This is Admin Home</a></h3>
+        </div>
+        <div class="container-fluid">
+            <div class="row g-3 mt-3">
+                <div class="col-md-4 offset-md-4">
+                    <div class="btn-group">
+                        <button @click="$router.push('/admin/user')" class="btn btn-outline-danger py-3">
+                            <h6>회원 관리</h6>
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-4 offset-md-4">
-                <div class="btn-group">
-                    <button @click="$router.push('/admin/post')" class="btn btn-outline-danger py-3">
-                        <h6>게시글 관리</h6>
-                    </button>
+                <div class="col-md-4 offset-md-4">
+                    <div class="btn-group">
+                        <button @click="$router.push('/admin/post')" class="btn btn-outline-danger py-3">
+                            <h6>게시글 관리</h6>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
