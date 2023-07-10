b2fd829784b537bb468b37b512c0e941ba0832ea
jae woo Jung <awfjol2008@naver.com>
2023-07-10 22:08:47 +0900

    feat: Navbar가 모든 페이지에 나타나도록 구현

--- vue/frontend/src/App.vue
+++ vue/frontend/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
+    <Navbar/>
     <router-view :key="$route.fullPath" />
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 
 export default {
   name: 'App',
   components: {
+    Navbar
   }
 }
 </script>
