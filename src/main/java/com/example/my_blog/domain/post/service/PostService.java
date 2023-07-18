7b1745172edfe04313075a51898e1872b2c69d5c
jae woo Jung <awfjol2008@naver.com>
2023-07-18 23:35:29 +0900

    feat: PostService 중복 좋아요 판별 메소드 구현

--- src/main/java/com/example/my_blog/domain/post/service/PostService.java
+++ src/main/java/com/example/my_blog/domain/post/service/PostService.java
@@ -1,9 +1,12 @@
 package com.example.my_blog.domain.post.service;
 
+import com.example.my_blog.domain.like.post.PostLike;
+import com.example.my_blog.domain.like.post.repository.PostLikeRepository;
 import com.example.my_blog.domain.post.Post;
 import com.example.my_blog.domain.post.repository.PostRepository;
 import com.example.my_blog.domain.post.service.dto.request.UpdatePostRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -59,4 +62,14 @@ public class PostService {
   }
 
 
+  // 이미 좋아요한 게시글인지 판별
+  public boolean isAlreadyLiked(Long postId, Long userId) {
+
+    PostLike postLike = postLikeRepository.getPostLike(postId, userId);
+    if (postLike == null) {
+      return false;
+    }
+
+    return true;
+  }
 }
