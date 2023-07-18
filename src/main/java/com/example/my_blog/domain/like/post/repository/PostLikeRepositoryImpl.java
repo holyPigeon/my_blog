67945fd0de9d8016264fe36a51a26c4172b01c6e
jae woo Jung <awfjol2008@naver.com>
2023-07-18 23:33:13 +0900

    feat: PostLikeRepositoryImpl 구현

---
+++ src/main/java/com/example/my_blog/domain/like/post/repository/PostLikeRepositoryImpl.java
@@ -0,0 +1,64 @@
+package com.example.my_blog.domain.like.post.repository;
+
+import com.example.my_blog.domain.like.post.PostLike;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostLikeRepositoryImpl implements PostLikeRepository {
+
+  private final EntityManager em;
+
+  @Override
+  public void save(PostLike postLike) {
+
+    em.persist(postLike);
+  }
+
+  @Override
+  public PostLike getPostLike(Long postId, Long userId) {
+
+    try { // 만약 해당 게시물에 해당 유저가 좋아요를 했다면 PostLike 객체 반환
+      return em.createQuery("select distinct pl from PostLike pl" +
+              " join fetch pl.post p" +
+              " join fetch pl.user u where p.id = :postId and u.id = :userId", PostLike.class)
+          .setParameter("postId", postId)
+          .setParameter("userId", userId)
+          .getSingleResult();
+    } catch (NoResultException e) { // 만약 해당 게시물에 해당 유저가 좋아요를 하지 않았다면 null 반환
+      return null;
+    }
+
+  }
+
+
+  @Override
+  public List<PostLike> getPostLikeListByPostId(Long postId) {
+
+    return em.createQuery("select distinct pl from PostLike pl" +
+            " join fetch pl.post p where p.id = :postId", PostLike.class)
+        .setParameter("postId", postId)
+        .getResultList();
+  }
+
+  @Override
+  public Long getPostLikeCountByPostId(Long postId) {
+
+    return em.createQuery("select distinct count(pl) from Post p" +
+        " join fetch p.likes pl where p.id = :postId", Long.class)
+        .setParameter("postId", postId)
+        .getSingleResult();
+  }
+
+  @Override
+  public void delete(Long postId, Long userId) {
+
+    PostLike postLike = getPostLike(postId, userId);
+    em.remove(postLike);
+  }
+}
