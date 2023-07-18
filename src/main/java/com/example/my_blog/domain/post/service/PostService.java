81008265dcc2dc3846200bf18378e9daec75215a
jae woo Jung <awfjol2008@naver.com>
2023-07-18 23:36:39 +0900

    feat: PostService 좋아요 및 좋아요 취소 메소드 구현

--- src/main/java/com/example/my_blog/domain/post/service/PostService.java
+++ src/main/java/com/example/my_blog/domain/post/service/PostService.java
@@ -5,6 +5,8 @@ import com.example.my_blog.domain.like.post.repository.PostLikeRepository;
 import com.example.my_blog.domain.post.Post;
 import com.example.my_blog.domain.post.repository.PostRepository;
 import com.example.my_blog.domain.post.service.dto.request.UpdatePostRequest;
+import com.example.my_blog.domain.user.User;
+import com.example.my_blog.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,9 +18,12 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
 
   private final PostRepository postRepository;
+  private final UserRepository userRepository;
+  private final PostLikeRepository postLikeRepository;
 
   @Transactional
   public Long save(Post item) {
@@ -61,6 +66,27 @@ public class PostService {
     postRepository.deleteById(id);
   }
 
+  @Transactional
+  public void likePost(Long postId, Long userId) {
+
+    if (isAlreadyLiked(postId, userId)) { // 이미 좋아요한 게시글이라면 에러 반환
+      throw new IllegalStateException("이미 좋아요한 게시글을 좋아요할 수 없습니다.");
+    }
+    Post post = postRepository.findById(postId);
+    User user = userRepository.findById(userId);
+    postLikeRepository.save(PostLike.createPostLike(user, post));
+  }
+
+  @Transactional
+  public void cancelLikePost(Long postId, Long userId) {
+
+    if (!isAlreadyLiked(postId, userId)) { // 이미 좋아요한 게시글이라면 에러 반환
+      throw new IllegalStateException("좋아요하지 않은 게시글의 좋아요를 취소할 수 없습니다.");
+    }
+    postLikeRepository.delete(postId, userId);
+  }
+
+  //== 게시글 좋아요 관련 조건 메소드 ==//
 
   // 이미 좋아요한 게시글인지 판별
   public boolean isAlreadyLiked(Long postId, Long userId) {
