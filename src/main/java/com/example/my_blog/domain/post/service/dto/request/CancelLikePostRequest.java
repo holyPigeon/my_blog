228ddc2b7cd1936bf9a041dcf2dadb405b20e93d
jae woo Jung <awfjol2008@naver.com>
2023-07-19 17:46:01 +0900

    feat: 게시글 좋아요 취소 request DTO 구현

---
+++ src/main/java/com/example/my_blog/domain/post/service/dto/request/CancelLikePostRequest.java
@@ -0,0 +1,15 @@
+package com.example.my_blog.domain.post.service.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CancelLikePostRequest {
+
+  private Long postId;
+
+  private Long userId;
+}
