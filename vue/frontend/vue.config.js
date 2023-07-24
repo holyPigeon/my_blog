662cebdaf6f252ffb1749d401987d28225b5a196
jae woo Jung <awfjol2008@naver.com>
2023-07-24 20:49:12 +0900

    feat: vue.config.js vuetify 관련 플러그인 설정 추가

--- vue/frontend/vue.config.js
+++ vue/frontend/vue.config.js
@@ -4,10 +4,23 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+
   devServer: {
     proxy : 'http://localhost:8080',
   },
-  indexPath: '../../templates/vue/index.html', // /static/vue 기준 -> outputDir을 기준으로 함
-  publicPath: '/vue', // 배포 url인 outputDir 경로인 vue 폴더를 가리킴
-  outputDir: path.resolve(__dirname, "../../src/main/resources/static/vue"), // blog 프로젝트 폴더 기준
+
+  // /static/vue 기준 -> outputDir을 기준으로 함
+  indexPath: '../../templates/vue/index.html',
+
+  // 배포 url인 outputDir 경로인 vue 폴더를 가리킴
+  publicPath: '/vue',
+
+  // blog 프로젝트 폴더 기준
+  outputDir: path.resolve(__dirname, "../../src/main/resources/static/vue"),
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  }
 })
