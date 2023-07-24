3b839e829709f486481caba344eaa32e83e97e4d
jae woo Jung <awfjol2008@naver.com>
2023-07-24 20:17:39 +0900

    feat: 게시글 좋아요 개수 response DTO 구현

---
+++ src/main/java/com/example/my_blog/domain/post/service/dto/response/GetPostLikeCountResponse.java
@@ -0,0 +1,13 @@
+package com.example.my_blog.domain.post.service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetPostLikeCountResponse {
+
+  private int count;
+}
