5d5e8c1cba7eb2390861a5368a38416d932ece0c
jae woo Jung <awfjol2008@naver.com>
2023-07-26 23:20:48 +0900

    feat: Tailwind 관련 config 파일 추가

--- vue/frontend/tailwind.config.js
+++ vue/frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content:[
+    "./public/index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
