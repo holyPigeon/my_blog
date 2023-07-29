42397ebdcc6073d985a22000791908b62cd9dec9
jae woo Jung <awfjol2008@naver.com>
2023-07-29 16:23:41 +0900

    feat: 게시글 목록 페이지 UI 변경

--- vue/frontend/src/views/post/PostList.vue
+++ vue/frontend/src/views/post/PostList.vue
@@ -1,56 +1,32 @@
 <template>
-    <div id="wrapper" class="container-fluid">
-        <div class="container px-4s my-5">
-            <div class="row gx-4 justify-content-center m-auto">
-                <!-- <div class="col-md-10 position-static d-block p-3 text-black"
-                    style="--bs-bg-opacity: .4; --bs-text-opacity: 0.6;">
-                    <div class="p-4">
-                        <h4 class="mb-3 text-center text-black fs-3 fw-bold">게시글 목록</h4>
-                    </div>
-
-                    <table class="text-center fs-4" style="width: 100%; border: 1px solid #444444;">
-                        <tr>
-                            <td>Post Id</td>
-                            <td>Post Author</td>
-                            <td>Post Title</td>
-                            <td>Post Content</td>
-                        </tr>
-                        <tr v-for="(post, index) in postList.postData" :key="index">
-                            <td>{{ post.id }}</td>
-                            <td>{{ post.author }}</td>
-                            <td>{{ post.title }}</td>
-                            <td>{{ post.content }}</td>
-                        </tr>
-                    </table>
-                </div> -->
-                <div class="col-md-10 position-static d-block p-3 text-black"
-                    style="--bs-bg-opacity: .4; --bs-text-opacity: 0.6;">
-                    <div class="p-4">
-                        <h4 class="mb-3 text-center text-black fs-3 fw-bold">게시글 목록</h4>
-                    </div>
-                    <table class="text-center" style="width: 100%; border: 1px solid #CCCDCE;">
+    <div class="hero min-h-screen bg-gradient-to-r from-sky-500 to-emerald-500">
+        <div class="bg-opacity-60 hero-overlay"></div>
+        <div class="hero-content text-center flex w-3/4">
+            <div class="flex-shrink-0 max-w-4xl w-full shadow-2xl bg-base-100 mb-64 p-8">
+                <div class="overflow-x-auto">
+                    <table class="table border border-base-content">
+                        <!-- head -->
                         <thead>
-                            <tr>
-                                <th>번호</th>
+                            <tr class="border border-base-content">
+                                <th></th>
                                 <th>제목</th>
                                 <th>글쓴이</th>
                                 <th>등록일</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="(post, index) in posts.postList" :key="index">
-                                <td style="width: 10%;">{{ post.id }}</td>
-                                <td style="width: 50%;"><a @click="$router.push(`/post/list/${post.id}`)"
-                                        class="custom-nav">{{ post.title }}</a></td>
+                            <tr v-for="(post, index) in posts.postList" :key="index" class="hover text-neutral-content ">
+                                <th style="width: 10%;">{{ post.id }}</th>
+                                <td style="width: 50%;">
+                                    <a @click="$router.push(`/post/list/${post.id}`)">
+                                        {{ post.title }}
+                                    </a>
+                                </td>
                                 <td style="width: 15%;">{{ post.author }}</td>
                                 <td>{{ post.createdAt }}</td>
                             </tr>
                         </tbody>
                     </table>
-
-                    <!-- <div class="row">
-                            <hr class="my-4 col-md-12 offset-md-0 border border-1 border-dark" style="opacity: 0.1;">
-                        </div> -->
                 </div>
             </div>
         </div>
@@ -86,7 +62,7 @@ export default {
             .then((res) => {
                 console.log(res);
                 // JSON.stringify("res => " + res);
-                this.posts = {...res.data};
+                this.posts = { ...res.data };
             }).catch((err) => {
                 JSON.stringify("err => " + err);
             });
