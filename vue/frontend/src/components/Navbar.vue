a569405bd3cbf5db4c872dd8a690f030e170b63b
jae woo Jung <awfjol2008@naver.com>
2023-07-29 16:19:11 +0900

    feat: Navigation bar 전체 UI 변경

--- vue/frontend/src/components/Navbar.vue
+++ vue/frontend/src/components/Navbar.vue
@@ -1,15 +1,40 @@
 <template>
-    <nav id="main-navbar" class="fixed-top navbar navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a @click="$router.push('/');" class="navbar-brand" href="#">
-                <img src="../assets/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
-                My Blog
+    <div class="navbar bg-primary">
+        <div class="flex-1">
+            <a @click="$router.push('/');" class="btn btn-ghost normal-case text-xl">
+                <p class="mb-1">myBlog</p>
             </a>
-
-            <!-- Right links -->
-            <div>
-                <ul class="navbar-nav d-flex align-items-center flex-row me-auto mb-2 mb-lg-0">
-
+        </div>
+        <div class="flex-none">
+            <!-- <div class="dropdown dropdown-end">
+                    <label tabindex="0" class="btn btn-ghost btn-circle">
+                        <div class="indicator">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                            <span class="badge badge-sm indicator-item">8</span>
+                        </div>
+                    </label>
+                    <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-secondary shadow">
+                        <div class="card-body">
+                            <span class="font-bold text-lg">8 Items</span>
+                            <span class="text-info">Subtotal: $999</span>
+                            <div class="card-actions">
+                                <button class="btn btn-primary btn-block">View cart</button>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+            <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img v-if="!isLoggedIn" :src="require('../assets/profile2.png')" />
+                        <img v-if="isLoggedIn" :src="require('../assets/profile3.png')" />
+                    </div>
+                </label>
+                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-primary rounded-box w-52">
                     <!-- 비로그인 시 -->
                     <div v-if="!isLoggedIn" class="d-flex">
                         <li class="nav-item">
