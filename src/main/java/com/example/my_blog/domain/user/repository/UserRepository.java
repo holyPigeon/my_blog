2c9c75efd8dc85d8e142c799629d7336b711fb48
jae woo Jung <awfjol2008@naver.com>
2023-08-25 19:10:19 +0900

    feat: UserRepository 검색 메소드 Optional 적용

--- src/main/java/com/example/my_blog/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/my_blog/domain/user/repository/UserRepository.java
@@ -3,18 +3,19 @@ package com.example.my_blog.domain.user.repository;
 import com.example.my_blog.domain.user.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository {
 
   void save(User user);
 
-  User findById(Long id);
+  Optional<User> findById(Long id);
 
-  User findByLoginId(String loginId);
+  Optional<User> findByLoginId(String loginId);
 
-  User findByName(String name);
+  Optional<User> findByName(String name);
 
-  User findByNickname(String nickname);
+  Optional<User> findByNickname(String nickname);
 
   List<User> findAll();
 
