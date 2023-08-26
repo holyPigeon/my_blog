ee2defc56e327abaa7951f9f5004be6dc2048e72
jae woo Jung <awfjol2008@naver.com>
2023-08-26 19:22:11 +0900

    feat: PostLikeRepository 검색 메소드 Optional 적용

--- src/main/java/com/example/my_blog/domain/like/post/repository/PostLikeRepository.java
+++ src/main/java/com/example/my_blog/domain/like/post/repository/PostLikeRepository.java
@@ -3,12 +3,13 @@ package com.example.my_blog.domain.like.post.repository;
 import com.example.my_blog.domain.like.post.PostLike;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostLikeRepository {
 
   void save(PostLike postLike);
 
-  PostLike getPostLike(Long postId, Long userId);
+  Optional<PostLike> getPostLike(Long postId, Long userId);
 
   List<PostLike> getPostLikeListByPostId(Long postId);
 
