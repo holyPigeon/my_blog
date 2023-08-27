9784024c411d9bf65fb4dec5bc20163a142f97b4
jae woo Jung <awfjol2008@naver.com>
2023-08-28 00:08:05 +0900

    feat: PostService 변경된 에러코드 적용

--- src/main/java/com/example/my_blog/domain/post/service/PostService.java
+++ src/main/java/com/example/my_blog/domain/post/service/PostService.java
@@ -7,6 +7,7 @@ import com.example.my_blog.domain.post.repository.PostRepository;
 import com.example.my_blog.domain.post.service.dto.request.UpdatePostRequest;
 import com.example.my_blog.domain.user.User;
 import com.example.my_blog.domain.user.repository.UserRepository;
+import com.example.my_blog.exception.MyBlogException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
+
+import static com.example.my_blog.exception.MyBlogErrorCode.*;
 
 @Service
 @Transactional(readOnly = true)
@@ -36,9 +38,8 @@ public class PostService {
 
   public Post findById(Long id) {
 
-    return postRepository.findById(id).orElseThrow(
-        () -> new NoSuchElementException("해당 게시글이 존재하지 않습니다.")
-    );
+    return postRepository.findById(id)
+        .orElseThrow(() -> MyBlogException.type(POST_NOT_FOUND));
   }
 
   public List<Post> findByUserId(Long userId) {
@@ -73,15 +74,13 @@ public class PostService {
   public void likePost(Long postId, Long userId) {
 
     if (isAlreadyLiked(postId, userId)) { // 이미 좋아요한 게시글이라면 에러 반환
-      throw new IllegalStateException("이미 좋아요한 게시글을 좋아요할 수 없습니다.");
+      throw MyBlogException.type(ALREADY_LIKE_MARKING);
     }
 
-    Post post = postRepository.findById(postId).orElseThrow(
-        () -> new NoSuchElementException("해당 게시글이 존재하지 않습니다.")
-    );
-    User user = userRepository.findById(userId).orElseThrow(
-        () -> new NoSuchElementException("해당 사용자가 존재하지 않습니다.")
-    );
+    Post post = postRepository.findById(postId)
+        .orElseThrow(() -> MyBlogException.type(POST_NOT_FOUND));
+    User user = userRepository.findById(userId)
+        .orElseThrow(() -> MyBlogException.type(USER_NOT_FOUND));
 
     postLikeRepository.save(PostLike.createPostLike(user, post));
   }
@@ -90,7 +89,7 @@ public class PostService {
   public void cancelLikePost(Long postId, Long userId) {
 
     if (!isAlreadyLiked(postId, userId)) { // 좋아요하지 않은 게시글이라면 에러 반환
-      throw new IllegalStateException("좋아요하지 않은 게시글의 좋아요를 취소할 수 없습니다.");
+      throw MyBlogException.type(ILLEGAL_LIKE_MARKING_CANCEL);
     }
     postLikeRepository.delete(postId, userId);
   }
