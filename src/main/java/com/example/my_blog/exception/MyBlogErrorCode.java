ef538484fac103b888da908e0d69c48609a5712c
jae woo Jung <awfjol2008@naver.com>
2023-08-28 19:20:36 +0900

    feat: 예외 발생 시 에러 코드 소폭 수정

--- src/main/java/com/example/my_blog/exception/MyBlogErrorCode.java
+++ src/main/java/com/example/my_blog/exception/MyBlogErrorCode.java
@@ -19,18 +19,11 @@ public enum MyBlogErrorCode {
   // Post
   POST_NOT_FOUND(HttpStatus.NOT_FOUND, "작품 정보가 존재하지 않습니다"),
   ART_SOLD_OUT(HttpStatus.BAD_REQUEST, "이미 판매 완료된 작품입니다"),
-  DUPLICATE_ART_NAME(HttpStatus.CONFLICT, "중복된 작품명입니다"),
-  ALREADY_AUCTION_PROCESS(HttpStatus.CONFLICT, "입찰이 이미 진행된 작품입니다"),
   ILLEGAL_ART_UPLOAD(HttpStatus.BAD_REQUEST, "작품 파일이 업로드되지 않았습니다"),
   ALREADY_LIKE_MARKING(HttpStatus.BAD_REQUEST, "이미 좋아요한 글입니다"),
-  ILLEGAL_LIKE_MARKING(HttpStatus.BAD_REQUEST, "자신의 작품을 찜할 수 없습니다"),
-  ILLEGAL_LIKE_MARKING_CANCEL(HttpStatus.BAD_REQUEST, "이미 찜을 취소하였거나 찜을 한 적이 없는 작품입니다"),
-
-  // Auction
-  AUCTION_NOT_FOUND(HttpStatus.NOT_FOUND, "경매 정보가 존재하지 않습니다"),
-  BID_AMOUNT_NOT_ENOUGH(HttpStatus.BAD_REQUEST, "입찰 금액이 부족합니다"),
-  CLOSED_AUCTION(HttpStatus.BAD_REQUEST, "이미 종료된 경매입니다"),
-  NOT_CLOSED_AUCTION(HttpStatus.BAD_REQUEST, "종료되지 않은 경매입니다"),
+  ILLEGAL_LIKE_MARKING_CANCEL(HttpStatus.BAD_REQUEST, "이미 좋아요를 취소하였거나 좋아요를 한 적이 없는 작품입니다"),
+  COMMENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "댓글 정보가 존재하지 않습니다."),
+  PARENT_COMMENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "부모 댓글 정보가 존재하지 않습니다."),
 
   // Point
   POINT_NOT_ENOUGH(HttpStatus.CONFLICT, "포인트가 부족합니다"),
