7a5613a9b52d2f03a5064371433a54ed8c725317
jae woo Jung <awfjol2008@naver.com>
2023-08-31 18:17:48 +0900

    feat: MyBlogException 클래스 내 주석 추가

--- src/main/java/com/example/my_blog/exception/MyBlogException.java
+++ src/main/java/com/example/my_blog/exception/MyBlogException.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 public class MyBlogException extends RuntimeException {
   private MyBlogErrorCode errorCode;
 
-  private MyBlogException(MyBlogErrorCode errorCode) {
+  private MyBlogException(MyBlogErrorCode errorCode) { // MyBlogException 생성자. 매개된 에러 코드에 따른 메시지와 에러 코드를 지정한다.
     super(errorCode.getMessage());
     this.errorCode = errorCode;
   }
 
-  public static MyBlogException type(MyBlogErrorCode errorCode) {
+  public static MyBlogException type(MyBlogErrorCode errorCode) { // 해당 에러 코드에 따라 새로운 MyBlogException 객체를 생성한다.
     return new MyBlogException(errorCode);
   }
 }
