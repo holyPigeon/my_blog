6d6ee7ea9c6ce9ef00ad11baf2b2982547795fa3
jae woo Jung <awfjol2008@naver.com>
2023-09-05 12:40:11 +0900

    feat: Spring Data JPA 기반 UserRepository 적용을 위한 기본 repository 인터페이스 및 구현체 삭제

--- src/main/java/com/example/my_blog/domain/user/repository/UserRepositoryJPA.java
+++
@@ -1,7 +0,0 @@
-package com.example.my_blog.domain.user.repository;
-
-import com.example.my_blog.domain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepositoryJPA extends JpaRepository<User, Long> {
-}
