48dd5138666342e1263c113cab74999bde828a9e
jae woo Jung <awfjol2008@naver.com>
2023-09-05 12:40:33 +0900

    feat: Spring Data JPA 기반 repository 적용

--- src/main/java/com/example/my_blog/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/my_blog/domain/user/repository/UserRepository.java
@@ -1,23 +1,17 @@
 package com.example.my_blog.domain.user.repository;
 
 import com.example.my_blog.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository {
-
-  void save(User user);
-
-  Optional<User> findById(Long id);
+public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByLoginId(String loginId);
 
   Optional<User> findByName(String name);
 
   Optional<User> findByNickname(String nickname);
-
-  List<User> findAll();
 
   void deleteById(Long id);
 }
