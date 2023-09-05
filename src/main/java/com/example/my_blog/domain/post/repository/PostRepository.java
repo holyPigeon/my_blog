56e4f0b226147b8ba4ef66733569436d3053d3f5
jae woo Jung <awfjol2008@naver.com>
2023-09-05 13:42:29 +0900

    feat: Spring Data JPA 기반 PostRepository 적용

--- src/main/java/com/example/my_blog/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/my_blog/domain/post/repository/PostRepository.java
@@ -1,19 +1,14 @@
 package com.example.my_blog.domain.post.repository;
 
 import com.example.my_blog.domain.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface PostRepository {
-
-  void save(Post post);
-
-  Optional<Post> findById(Long id);
+public interface PostRepository extends JpaRepository<Post, Long> {
 
   List<Post> findByUserId(Long memberId);
-
-  List<Post> findAll();
 
   void deleteById(Long id);
 }
