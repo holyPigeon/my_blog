45bf6cb8e753165bb6546e0030c109bf3865d624
jae woo Jung <awfjol2008@naver.com>
2023-09-06 21:51:06 +0900

    feat: PostRepository findByUserId 메소드 EntityGraph 적용
    jpql의 fetch join 역할을 수행하는 EntityGraph 어노테이션 적용

--- src/main/java/com/example/my_blog/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/my_blog/domain/post/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.example.my_blog.domain.post.repository;
 
 import com.example.my_blog.domain.post.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+  @EntityGraph(attributePaths = {"user"})
   List<Post> findByUserId(Long memberId);
 
   void deleteById(Long id);
