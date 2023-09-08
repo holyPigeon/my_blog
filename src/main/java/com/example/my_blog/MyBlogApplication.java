1f8b8b7bcdaecb2976d33bd3130ea4c83670df12
jae woo Jung <awfjol2008@naver.com>
2023-09-08 11:16:51 +0900

    feat: JPA Auditing 적용을 위한 메인 어플리케이션 로직 구현

--- src/main/java/com/example/my_blog/MyBlogApplication.java
+++ src/main/java/com/example/my_blog/MyBlogApplication.java
@@ -2,12 +2,24 @@ package com.example.my_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class MyBlogApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MyBlogApplication.class, args);
+  }
+
+  @Bean
+  public AuditorAware<String> auditorProvider() {
+    return () -> Optional.of(UUID.randomUUID().toString());
   }
 
 }
