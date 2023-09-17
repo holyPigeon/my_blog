5dda2944b99810c3c18764fc03ff34bf369e942b
jae woo Jung <awfjol2008@naver.com>
2023-09-17 23:10:06 +0900

    feat: UserRepository findAll 메소드 반환 타입 Page<User>로 변경

--- src/main/java/com/example/my_blog/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/my_blog/domain/user/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.my_blog.domain.user.repository;
 
 import com.example.my_blog.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  Page<User> findAll(Pageable pageable);
 
   Optional<User> findByLoginId(String loginId);
 
