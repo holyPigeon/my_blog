676616e47c44e9c7227a406f6cc845f248df8a80
jae woo Jung <awfjol2008@naver.com>
2023-09-18 00:44:07 +0900

    feat: 회원 목록 조회 API 반환값이 Page 타입으로 변경됨에 따라 본래의 회원 목록 DTO 비활성화

--- src/main/java/com/example/my_blog/domain/user/service/dto/response/ListUserResponse.java
+++ src/main/java/com/example/my_blog/domain/user/service/dto/response/ListUserResponse.java
@@ -1,14 +1,14 @@
-package com.example.my_blog.domain.user.service.dto.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ListUserResponse<T> {
-
-  private int count;
-  private T data;
-}
+//package com.example.my_blog.domain.user.service.dto.response;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class ListUserResponse<T> {
+//
+//  private long count;
+//  private T data;
+//}
