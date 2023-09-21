606b3d3e37eeb80be63fe3b001888a8a94230295
jae woo Jung <awfjol2008@naver.com>
2023-09-21 22:12:15 +0900

    feat: UserRepository Page<Post> 타입의 findAll 메소드 추가

--- src/main/java/com/example/my_blog/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/my_blog/domain/post/repository/PostRepository.java
@@ -1,13 +1,15 @@
 package com.example.my_blog.domain.post.repository;
 
 import com.example.my_blog.domain.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+  Page<Post> findAll(Pageable pageable);
 
   @EntityGraph(attributePaths = {"user"})
   Page<Post> findByUserId(Long memberId, Pageable pageable);
