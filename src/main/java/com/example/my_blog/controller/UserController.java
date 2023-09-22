a4a122578df05b30360a3554c5d307a6da679a23
jae woo Jung <awfjol2008@naver.com>
2023-09-22 11:20:05 +0900

    refector: UserController 불필요한 참조 제거

--- src/main/java/com/example/my_blog/controller/UserController.java
+++ src/main/java/com/example/my_blog/controller/UserController.java
@@ -8,7 +8,6 @@ import com.example.my_blog.domain.user.service.dto.response.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
