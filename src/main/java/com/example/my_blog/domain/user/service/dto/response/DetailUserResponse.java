60be34c014b4a714c3cab86fee531847d5c5186a
jae woo Jung <awfjol2008@naver.com>
2023-09-23 19:16:46 +0900

    feat: 회원 상세 조회 response DTO User 타입 파라미터를 통한 생성자 구현

--- src/main/java/com/example/my_blog/domain/user/service/dto/response/DetailUserResponse.java
+++ src/main/java/com/example/my_blog/domain/user/service/dto/response/DetailUserResponse.java
@@ -1,5 +1,6 @@
 package com.example.my_blog.domain.user.service.dto.response;
 
+import com.example.my_blog.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,11 @@ public class DetailUserResponse {
   private Long id;
   private String name;
   private String nickname;
+
+  public DetailUserResponse(User user) {
+
+    this.id = user.getId();
+    this.name = user.getName();
+    this.nickname = user.getNickname();
+  }
 }
