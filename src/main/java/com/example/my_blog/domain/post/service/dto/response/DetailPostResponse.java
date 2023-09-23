adae65de6c3785b2e6fe2bc20df65d7283459a37
jae woo Jung <awfjol2008@naver.com>
2023-09-23 19:17:09 +0900

    feat: 게시글 상세 조회 response DTO Post 타입 파라미터를 통한 생성자 구현

--- src/main/java/com/example/my_blog/domain/post/service/dto/response/DetailPostResponse.java
+++ src/main/java/com/example/my_blog/domain/post/service/dto/response/DetailPostResponse.java
@@ -1,5 +1,6 @@
 package com.example.my_blog.domain.post.service.dto.response;
 
+import com.example.my_blog.domain.post.Post;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,4 +23,14 @@ public class DetailPostResponse {
 
   @JsonFormat(pattern = "yyyy.MM.dd HH:mm:ss")
   private LocalDateTime lastModifiedDate;
+
+  public DetailPostResponse (Post post) {
+
+    this.id = post.getId();
+    this.author = post.getUser().getNickname();
+    this.title = post.getTitle();
+    this.content = post.getContent();
+    this.createdDate = post.getCreatedDate();
+    this.lastModifiedDate = post.getLastModifiedDate();
+  }
 }
