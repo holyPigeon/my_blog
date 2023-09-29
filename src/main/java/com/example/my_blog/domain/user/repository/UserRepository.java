5b10de795fda496ed09b8d8947a58afb26da401f
jae woo Jung <awfjol2008@naver.com>
2023-09-29 21:49:50 +0900

    feat: UserRepository 인터페이스QueryDsl 커스텀 리포지토리 인터페이스 상속

--- src/main/java/com/example/my_blog/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/my_blog/domain/user/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.example.my_blog.domain.user.repository;
 
 import com.example.my_blog.domain.user.User;
+import com.example.my_blog.domain.user.repository.custom.UserQueryDslRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserQueryDslRepository {
 
   Page<User> findAll(Pageable pageable);
 
