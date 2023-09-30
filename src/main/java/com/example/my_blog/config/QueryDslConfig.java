5664c7be70b98330b25993addd31a34f6c0c364a
jae woo Jung <awfjol2008@naver.com>
2023-10-01 01:28:11 +0900

    feat: QueryDsl JPAQueryFactory 스프링 Bean 적용

---
+++ src/main/java/com/example/my_blog/config/QueryDslConfig.java
@@ -0,0 +1,18 @@
+package com.example.my_blog.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+  @PersistenceContext
+  private EntityManager em;
+
+  @Bean
+  JPAQueryFactory jpaQueryFactory() {
+    return new JPAQueryFactory(em);
+  }
+}
